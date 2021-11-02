47ea947e8e8f3147286f7ccc1a657e5e29a4afe5
SHADOWZXCV <faded.lover.shadows@gmail.com>
2021-11-02 15:49:40 +0000

    Added magento's graphql direct_product_add module

---
+++ app/code/ScandiPWA/DirectAddProductViaLinkGraphQl/Model/Resolver/GetProductSkuById.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+
+namespace ScandiPWA\DirectAddProductViaLinkGraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
+class GetProductSkuById implements ResolverInterface
+{
+
+    protected $productRepository;
+
+    public function __construct(
+        ProductRepositoryInterface $productRepository
+      ) {
+
+          $this->productRepository = $productRepository;
+      }
+
+    /**
+     * @param Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     *
+     * @return array|Value|mixed
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    )
+    {
+        $product = $this->productRepository->getById($args['product_id']);
+        return ['ProductSku' => $product->getSku()];
+    }
+}
