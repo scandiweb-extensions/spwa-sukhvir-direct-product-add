f9a0be5a57697c0ddb0172d4bdc6558814bd8436
SHADOWZXCV <faded.lover.shadows@gmail.com>
2021-11-02 15:30:29 +0000

    Moved extension from master to development

--- pwa/packages/spwa-sukhvir-direct-product-add/src/query/GetProductById.query.js
+++
@@ -1,6 +0,0 @@
-import { Field } from 'Util/Query';
-
-/** @namespace SpwaSukhvirDirectProductAdd/Query/GetProductById/Query/getProductByIdQuery */
-export const getProductByIdQuery = (productId) => (new Field('getProductSKUById')
-    .addArgument('product_id', 'Int', Number(productId))
-    .addField('ProductSku'));
