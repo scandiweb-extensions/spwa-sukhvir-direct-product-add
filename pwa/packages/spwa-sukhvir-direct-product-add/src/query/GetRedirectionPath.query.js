deb9754110168e103d80868c64e992a60936b254
Ezz-Aldyn Husen <34347098+SHADOWZXCV@users.noreply.github.com>
2021-11-26 16:18:16 +0200

    Create GetRedirectionPath.query.js

---
+++ pwa/packages/spwa-sukhvir-direct-product-add/src/query/GetRedirectionPath.query.js
@@ -0,0 +1,5 @@
+import { Field } from 'Util/Query';
+
+/** @namespace SpwaSukhvirDirectProductAdd/Query/GetRedirectionPath/Query/getProductByIdQuery */
+export const redirectionPathQuery = new Field('getRedirectRoute')
+    .addField('redirect_to');
